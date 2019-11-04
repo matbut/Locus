af1969ff886a34c99779778935c01131d9f18dec
almazur <26549279+almazur@users.noreply.github.com>
2019-11-04 13:36:58 +0100

    [LOCUS-14] [LOCUS-14] Retrieving date from google search result

--- googleCrawlerOfficial/crawler.py
+++ googleCrawlerOfficial/crawler.py
@@ -1,13 +1,13 @@
 import asyncio
 import json
 import logging
-from datetime import datetime
 from urllib import request
 from urllib.parse import quote
 
 from asgiref.sync import async_to_sync
 from channels.consumer import SyncConsumer
 
+from googleCrawlerOfficial import patterns
 from search.models import CrawlParameters
 from .models import GoogleResultOfficial
 
@@ -25,25 +25,20 @@ class Crawler(SyncConsumer):
         query_raw = crawl_parameters.title
         query = quote(query_raw.encode('utf8'))
 
-        key, engine_id = self.retrieve_access_key()
-        url = 'https://www.googleapis.com/customsearch/v1?key={0}&cx={1}&q={2}'.format(key, engine_id, query)
-        print("HTTP GET: ", url)
-        response = request.urlopen(url).read()
+        key, engine_id = patterns.retrieve_access_key()
+        response = request.urlopen('https://www.googleapis.com/customsearch/v1?key={0}&cx={1}&q={2}'
+                                   .format(key, engine_id, query)).read()
         response_json = json.loads(response)
-        print(response_json)
 
-        items = response_json["items"]
+        items = response_json['items']
         for item in items:
             page = item['displayLink']
-            date = datetime.utcfromtimestamp(100000000 / 1000.0).date()
-            #date = item['metatags'] find article:published_time
+            date = patterns.retrieve_date(item['snippet'])
             link = item['link']
             search_result = GoogleResultOfficial(page=page, date=date, link=link)
             search_result.save()
-            print(item)
 
         asyncio.set_event_loop(asyncio.new_event_loop())
-
 
         # Send message
         async_to_sync(self.channel_layer.group_send)(
@@ -53,13 +48,3 @@ class Crawler(SyncConsumer):
                 'message': 'google_crawler'
             }
         )
-
-    def retrieve_access_key(self):
-        from pathlib import Path
-        import re
-        home_dir = str(Path.home())
-        credentials = open("{0}/.locus/credentials".format(home_dir), "r")
-        content = credentials.read()
-        m = re.match(r"key (?P<key>\w+)\nengine_id (?P<engine_id>\w+:\w+)", content)
-        key = m.groupdict()
-        return key["key"], key["engine_id"]
--- googleCrawlerOfficial/models.py
+++ googleCrawlerOfficial/models.py
@@ -4,5 +4,12 @@ from django.db import models
 class GoogleResultOfficial(models.Model):
     id = models.IntegerField(primary_key=True)
     page = models.TextField()
-    date = models.DateField()
+    date = models.DateField(null=True)
     link = models.URLField()
+
+    @property
+    def get_date(self):
+        if self.date:
+            return self.date
+        else:
+            return "-"
