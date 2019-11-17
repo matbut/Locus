f4e52221103eb13d18ab03048dbd011f0ae660ae
matbur <butamateusz@gmail.com>
2019-11-17 10:21:19 +0100

    [Locus-16] VisJs network added and configured

--- Locus/settings.py
+++ Locus/settings.py
@@ -42,7 +42,9 @@ INSTALLED_APPS = [
     'googleCrawlerOfficial.apps.GoogleCrawlerOfficialConfig',
     'database.apps.DatabaseConfig',
     'channels',
-    'rest_framework'
+    'rest_framework',
+    'compressor',
+    'compressor_toolkit',
 ]
 
 MIDDLEWARE = [
@@ -124,13 +126,35 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
+MEDIA_ROOT = 'media'
+MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
     '/var/www/static/',
+    os.path.join(BASE_DIR, 'node_modules'),
 ]
 
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+)
+
+COMPRESS_CSS_FILTERS = [
+    'compressor.filters.css_default.CssAbsoluteFilter',
+    'compressor.filters.cssmin.CSSMinFilter',
+    'compressor.filters.template.TemplateFilter'
+]
+COMPRESS_JS_FILTERS = [
+    'compressor.filters.jsmin.JSMinFilter',
+]
+COMPRESS_PRECOMPILERS = (
+    ('module', 'compressor_toolkit.precompilers.ES6Compiler'),
+    ('css', 'compressor_toolkit.precompilers.SCSSCompiler'),
+)
+COMPRESS_ENABLED = True
 
 CHANNEL_LAYERS = {
     'default': {
