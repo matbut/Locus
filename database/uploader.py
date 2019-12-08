b4951bd3e98b30853bb42158939ca737f5f51830
almazur <26549279+almazur@users.noreply.github.com>
2019-12-08 20:13:37 +0100

    [LOCUS-34] Customizing file upload

--- database/uploader.py
+++ database/uploader.py
@@ -6,26 +6,26 @@ from common.url import get_domain, clean_url
 from database.models import ImportedArticle
 
 
-def upload(file_data):
+def upload(file_data, params):
     lines = file_data.splitlines()
     articles = []
-    csv_reader = get_csv_reader(lines[1:])
+    csv_reader = get_csv_reader(lines[1:], params['delimiter'])
     for fields in csv_reader:
-        article = get_article_from_field(fields)
+        article = get_article_from_field(fields, params)
         articles.append(article)
     save_articles(articles)
 
 
-def read_upload(file_name):
+def read_upload(file_name, params):
     articles = []
     with open(file_name) as csv_file:
-        csv_reader = get_csv_reader(csv_file)
+        csv_reader = get_csv_reader(csv_file, params['delimiter'])
         line_count = 0
         for fields in csv_reader:
             if line_count == 0:
                 line_count += 1
             else:
-                article = get_article_from_field(fields)
+                article = get_article_from_field(fields, params)
                 articles.append(article)
                 line_count += 1
     os.remove(file_name)
@@ -37,11 +37,11 @@ def save_articles(articles):
         article.save()
 
 
-def get_csv_reader(content):
-    return csv.reader(content, quotechar='"', delimiter=';', quoting=csv.QUOTE_ALL, skipinitialspace=True)
+def get_csv_reader(content, delimiter):
+    return csv.reader(content, quotechar='"', delimiter=delimiter, quoting=csv.QUOTE_ALL, skipinitialspace=True)
 
 
-def get_article_from_field(fields):
-    page = get_domain(fields[0])
-    return ImportedArticle(page=page, date=parse_date(fields[1], '%d %L, %Y godz. %H:%M'),
-                           link=clean_url(fields[0]), title=fields[3], content=fields[2])
+def get_article_from_field(fields, params):
+    page = get_domain(fields[params['link']])
+    return ImportedArticle(page=page, date=parse_date(fields[params['date']], params['pattern']),
+                           link=clean_url(fields[params['link']]), title=fields[params['title']], content=fields[params['content']])
